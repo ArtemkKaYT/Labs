66f028d719b74731e4f0a0bb6e1a9b299ded70b3
Artemka <korolev.artiom228@gmail.com>
2025-10-08 20:16:25 +0300

    Исправлен синтаксис

--- Lab1/1.py
+++ Lab1/1.py
@@ -5,7 +5,8 @@ GREEN = f'{CSI}42m'
 RED = f'{CSI}41m'
 YELLOW = f'{CSI}43m'
 
-for i in range(4):
+flag_size = 4
+for i in range(flag_size):
     if i <= 1:
         print(f'{GREEN}      {YELLOW}         {RESET}')
     else:

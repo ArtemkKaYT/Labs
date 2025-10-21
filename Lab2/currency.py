9dd45bc82ec45b689dfc547d0fa09e20bc8e6f9c
Artemka <korolev.artiom228@gmail.com>
2025-10-21 13:27:18 +0300

    Добавлен словарь

--- Lab2/currency.py
+++ Lab2/currency.py
@@ -1,13 +1,16 @@
 from xml.dom import minidom
 
 def parse(xml_file):
+    dictionary = {}
     data = minidom.parse(xml_file)
     valutes = data.getElementsByTagName('Valute')
     for string in valutes:
         char_code = string.getElementsByTagName('CharCode')[0].firstChild.nodeValue
         nominal = string.getElementsByTagName('Nominal')[0].firstChild.nodeValue
-        print(f'{char_code} - {nominal}')
+        dictionary[char_code] = int(nominal)
+    return dictionary
 
 if __name__ == '__main__':
     with open('currency.xml', encoding='windows-1251') as xml_file:
-        parse(xml_file)
+        dictionary = parse(xml_file)
+        print(dictionary)

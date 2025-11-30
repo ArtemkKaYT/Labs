488c2e1d7dffce51b639b7f871964f9f898f59a6
Artemka <korolev.artiom228@gmail.com>
2025-11-30 16:01:14 +0300

    добавлено задание 2

--- Lab5/task.py
+++ Lab5/task.py
@@ -2,16 +2,36 @@ import re
 
 
 FILE_1 = 'task1-ru.txt'
+FILE_2 = 'task2.html'
+FILE_3 = 'task3.txt'
 
 
 def match(pattern, text):
     return re.findall(pattern, text)
 
 
+def iter(pattern, text):
+    return re.finditer(pattern, text)
+
+
 with open(FILE_1, encoding='UTF-8') as file:
     text = file.read()
-    pattern_1 = r'рис\.\s*\d+'
+    pattern_1 = r'[Рр]ис\.\s*\d+'
     pattern_2 = r'\b[A-Za-zА-Яа-я]{4}\b'
     res = match(pattern_1, text) + match(pattern_2, text)
-    print(len(res))
+    print('Задание 1:', len(res))
 
+with open(FILE_2, encoding='UTF-8') as file:
+    text = file.read()
+    pattern_font = r"font-family:\s*'(.+)';"
+    pattern_style = r"font-style:\s*(.+);"
+    pattern_weight = r"font-weight:\s*(.+);"
+    fonts = match(pattern_font, text)
+    styles = match(pattern_style, text)
+    weight = match(pattern_weight, text)
+    print('Задание 2:')
+    for i in range(len(fonts)):
+        print(f'{fonts[i]} - {styles[i]}, {weight[i]}')
+
+with open(FILE_3, encoding='UTF-8') as file:
+    text = file.read()
